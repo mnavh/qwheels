47417651c907dd72e5972384472f8552adde3b5d
Naveed Hussain <naveed92@outlook.com>
2020-07-30 17:32:02 +0500

    Added Dictonary to HTML

--- QWheels/Qwheel_App/views.py
+++ QWheels/Qwheel_App/views.py
@@ -4,11 +4,15 @@ from django.contrib.auth.models import User
 from django.contrib.auth import login
 
 # Create your views here.
-#modified view filesdad
+
+list_topbar = [{"title":'Home',"link":''},{"title":'Vendors',"link":'index.html'},{"title":'Deals',"link":'deals.html'},{"title":'Track Order',"link":'track-order.html'},{"title":'Blog',"link":'index.html'},{"title":'Contact Us',"link":'about-us.html'},{"title":'About Us',"link":'about-us.html'}]
 
 def main_page(request):
     print("Rendering main page...")
-    return render(request, 'index.html', {})
+    context = {
+        'menu':list_topbar
+    }
+    return render(request, 'index.html', context)
 
 def deals_page(request):
     return render(request, 'deals.html', {})
