06466ef60740eb621d4d79aa95d710741845b069
unknown <qamar.abbas14@hotmail.com>
2020-07-31 19:28:31 +0500

    merged

--- QWheels/Qwheel_App/views.py
+++ QWheels/Qwheel_App/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import login, authenticate
 
 # Create your views here.
 
-list_topbar = [{"title":'Home',"link":''},{"title":'Vendors',"link":'index.html'},{"title":'Deals',"link":'deals.html'},{"title":'Track Order',"link":'track-order.html'},{"title":'Blog',"link":'index.html'},{"title":'Contact Us',"link":'about-us.html'},{"title":'About Us',"link":'about-us.html'}]
+list_topbar = [{"title":'Home',"link":''},{"title":'Vendors',"link":''},{"title":'Deals',"link":'deals'},{"title":'Track Order',"link":'track-order.html'},{"title":'Blog',"link":'index.html'},{"title":'Contact Us',"link":'about-us.html'},{"title":'About Us',"link":'about-us.html'}]
 
 def main_page(request):
     print("Rendering main page...")
@@ -15,6 +15,7 @@ def main_page(request):
     return render(request, 'index.html', context)
 
 def deals_page(request):
+    print("Deals page")
     return render(request, 'deals.html', {})
 
 def account_page(request):
